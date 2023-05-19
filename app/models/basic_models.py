c15f2609440a935899acbd31669a430eeb79e4d4
BETEPOK3 <veterok.email@mail.ru>
2023-05-19 15:39:39 +0300

    Created database

--- app/models/basic_models.py
+++ app/models/basic_models.py
@@ -1,15 +1,8 @@
-from sqlalchemy import MetaData, Integer, String, Column, Date, ARRAY, ForeignKey
+from sqlalchemy import MetaData, Integer, String, Column, Date, ForeignKey, ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
 Base = declarative_base()
-
-
-class Data(Base):
-    __tablename__ = 'data'
-    metadata = metadata
-    id = Column(Integer(), primary_key=True, autoincrement=True)
-    data = Column(String(), unique=True, nullable=False)
 
 
 class Article(Base):

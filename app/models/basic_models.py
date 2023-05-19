43fdca4b757ca7dc12914c5772bbac0464bb60c7
BETEPOK3 <veterok.email@mail.ru>
2023-05-19 15:17:16 +0300

    Init database

--- app/models/basic_models.py
+++ app/models/basic_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData,  Integer, String, Column,  Boolean
+from sqlalchemy import MetaData, Integer, String, Column, Date, ARRAY, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
@@ -10,3 +10,28 @@ class Data(Base):
     metadata = metadata
     id = Column(Integer(), primary_key=True, autoincrement=True)
     data = Column(String(), unique=True, nullable=False)
+
+
+class Article(Base):
+    __tablename__ = 'article'
+    metadata = metadata
+    id_article = Column(Integer(), primary_key=True, autoincrement=True)
+    article_authors = Column(ARRAY(String))
+    article_keywords = Column(ARRAY(String))
+    article_date = Column(Date(), nullable=False)
+    article_text = Column(String(), nullable=False)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    metadata = metadata
+    id_category = Column(Integer(), primary_key=True, autoincrement=True)
+    category_name = Column(String(), unique=True, nullable=False)
+
+
+class ArticleCategory(Base):
+    __tablename__ = 'article_category'
+    metadata = metadata
+    id_article_category = Column(Integer(), primary_key=True, autoincrement=True)
+    id_article = Column(ForeignKey("article.id_article", onupdate="RESTRICT", ondelete="RESTRICT"))
+    id_category = Column(ForeignKey("category.id_category", onupdate="RESTRICT", ondelete="RESTRICT"))

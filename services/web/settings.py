89ab6b6f7302c4e20566b6588233c6015b4eeca4
Fraiters <67577747+Fraiters@users.noreply.github.com>
2023-12-23 08:50:29 +0300

    feat-#976442: added: 1) exception_handler; 2) settings.py; 3) alembic version
    * feat-#976442: add exception_handler for
    
    * feat-#976442: added settings.py with CATEGORY_LIST, added alembic version with autofill of categories

---
+++ services/web/settings.py
@@ -0,0 +1,48 @@
+CATEGORY_LIST = \
+    [
+        {
+            "category_name": "Автомобили"
+        },
+        {
+            "category_name": "Аниме"
+        },
+        {
+            "category_name": "Игры"
+        },
+        {
+            "category_name": "Интернет"
+        },
+        {
+            "category_name": "Культура"
+        },
+        {
+            "category_name": "Манга"
+        },
+        {
+            "category_name": "Наука"
+        },
+        {
+            "category_name": "Общество"
+        },
+        {
+            "category_name": "Политика"
+        },
+        {
+            "category_name": "Происшествия"
+        },
+        {
+            "category_name": "Прочее"
+        },
+        {
+            "category_name": "Спорт"
+        },
+        {
+            "category_name": "Технологии"
+        },
+        {
+            "category_name": "Трейлер"
+        },
+        {
+            "category_name": "Экономика"
+        },
+    ]
